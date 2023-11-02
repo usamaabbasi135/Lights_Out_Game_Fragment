b1ef2602dc9b3a4b7e5e31ec976cee6304130ff4
Usama Bin Hafeez Abbasi <usamahafeez.abbasi1234@gmail.com>
2023-11-03 00:22:27 +0500

    Completed the project

---
+++ app/src/main/java/com/example/diceroller/light_out_game_fragments/usamaabbasi1835/HelpFragment.java
@@ -0,0 +1,15 @@
+package com.example.diceroller.light_out_game_fragments.usamaabbasi1835;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx. annotation.NonNull;
+import androidx. fragment.app.Fragment;
+
+public class HelpFragment extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_help, container, false);
+    }
+}
